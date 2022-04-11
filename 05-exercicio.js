84d450449031cf545fe310099208aae37fd5a459
Mariana Nunes <marinun47@gmail.com>
2022-04-11 00:34:35 -0300

    Adiciona resposta da questão 5

--- 05-exercicio.js
+++ 05-exercicio.js
@@ -1,1 +1,13 @@
 //5 - Crie uma função que recebe o ano de nascimento da pessoa informando se ela é maior de idade ou menor.
+
+function retornaMaioridade(anoNascimento, anoAtual) {
+  const idade = anoAtual - anoNascimento;
+  if (idade >= 18) {
+    return `É maior de idade.`;
+  } else {
+    return `É menor de idade.`;
+  }
+}
+
+console.log(retornaMaioridade(1998, 2022));
+console.log(retornaMaioridade(2005, 2022));
