893d46c0c5af60526d089d432c0f300f7e474859
Mariana Nunes <marinun47@gmail.com>
2022-04-11 00:32:57 -0300

    Adiciona resposta da questão 1

--- 01-exercicio.js
+++ 01-exercicio.js
@@ -1,1 +1,25 @@
 //1 - Elabore um algoritmo que receba um número (1-7) e devolva o dia da semana correspondente.
+
+function dizDiaDaSemana(num) {
+  switch (num) {
+    case 1:
+      return "Hoje é domingo.";
+    case 2:
+      return "Hoje é segunda-feira.";
+    case 3:
+      return "Hoje é terça-feira.";
+    case 4:
+      return "Hoje é quarta-feira.";
+    case 5:
+      return "Hoje é quinta-feira.";
+    case 6:
+      return "Hoje é sexta-feira.";
+    case 7:
+      return "Hoje é sábado.";
+    default:
+      return "Erro";
+  }
+}
+
+console.log(dizDiaDaSemana(7));
+console.log(dizDiaDaSemana(99));
