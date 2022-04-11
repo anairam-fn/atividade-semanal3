fadda29d3b241886c6639f6e74b0becade5ad574
Mariana Nunes <marinun47@gmail.com>
2022-04-11 00:35:21 -0300

    Adiciona resposta da questão 7

--- 07-exercicio.js
+++ 07-exercicio.js
@@ -1,1 +1,14 @@
-//7 - Crie uma função que recebe 2 parâmetros e retorna o resultado da divisão entre eles. Além disso, se o resto dessa divisão for zero deverá imprimir no console o valor e dizer que ele é par.
+//7 - Crie uma função que recebe 2 parâmetros e retorna o resultado da divisão entre eles. Além disso,
+//se o resto dessa divisão for zero deverá imprimir no console o valor e dizer que ele é par.
+
+function dividirNumeros(num1, num2) {
+  const divisao = num1 / num2;
+  if (num1 % num2 == 0) {
+    return `O resultado da divisão entre ${num1} e ${num2} é inteiro (${divisao}).`;
+  } else {
+    return `O resultado da divisão entre ${num1} e ${num2} é decimal (${divisao}).`;
+  }
+}
+
+console.log(dividirNumeros(40, 10));
+console.log(dividirNumeros(49, 5));
