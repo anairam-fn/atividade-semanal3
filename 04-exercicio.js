45d58aa8932a79f8601af1f5cd61bd3568ad26a0
Mariana Nunes <marinun47@gmail.com>
2022-04-11 00:34:12 -0300

    Adiciona resposta da questão 4

--- 04-exercicio.js
+++ 04-exercicio.js
@@ -1,1 +1,11 @@
 //4 - Crie uma função que retorna a palavra (impar/par) de acordo com seu parâmetro.
+
+function imparPar(num) {
+  if (num % 2 == 0) {
+    return `O número ${num} é par.`;
+  } else {
+    return `O número ${num} é ímpar.`;
+  }
+}
+
+console.log(imparPar(100));
