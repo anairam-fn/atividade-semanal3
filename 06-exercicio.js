53004f445d829ce6001973118aaedfbf14b970db
Mariana Nunes <marinun47@gmail.com>
2022-04-11 00:34:56 -0300

    Adiciona resposta da questão 6

--- 06-exercicio.js
+++ 06-exercicio.js
@@ -1,1 +1,8 @@
 //6 - crie um algoritmo que converte dias em horas, quando recebe um número de dias.
+
+function conversorDiasHoras (dias) {
+    const horas = dias * 24
+    return `${dias} equivalem a ${horas} horas.`
+}
+
+console.log(conversorDiasHoras(10))
