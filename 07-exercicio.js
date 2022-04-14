53cd65c71e88e55edc2fc01e7cedbbfc32778d32
Mariana Nunes <marinun47@gmail.com>
2022-04-14 16:13:51 -0300

    Altera enunciado da questão 7

--- 07-exercicio.js
+++ 07-exercicio.js
@@ -1,5 +1,5 @@
-//7 - Crie uma função que recebe 2 parâmetros e retorna o resultado da divisão entre eles. Além disso,
-//se o resto dessa divisão for zero deverá imprimir no console o valor e dizer que ele é par.
+//7 - Crie uma função que recebe 2 parâmetros e retorna o resultado da divisão entre eles. Diga se
+// número é par ou ímpar.
 
 function dividirNumeros(num1, num2) {
   const divisao = num1 / num2;
